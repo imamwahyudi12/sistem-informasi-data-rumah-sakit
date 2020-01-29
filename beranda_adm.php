e66478fa10184f86df6c28c077732fe193c66b93
imamwahyudi12 <60435579+imamwahyudi12@users.noreply.github.com>
2020-01-29 22:32:18 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat Datang <strong><?=$_SESSION['nama']?></strong> di Sistem Informasi Data Pasien
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><center>Halaman Administrator Sistem Informasi Data Pasien Rumah Sakit</center></h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img width = "300" height="300" src="img/polilogo.JPG"></img></p>
+                          <p class="label-title" align="center"><strong>RSUD H. ABDUL MANAN SIMATUPANG</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Data Pasien.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data pasien masuk, pengelolaan data pasien keluar, <br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data pasien masuk dan data pesien keluar.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
